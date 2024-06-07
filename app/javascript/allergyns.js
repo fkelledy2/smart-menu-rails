99e13c8a494fd0a774192280a233bbd6ddcc57ac
fkelledy2 <fkelledy@gmail.com>
2024-06-07 23:24:53 +0100

    Update menu config layout

--- app/javascript/allergyns.js
+++ app/javascript/allergyns.js
@@ -1,6 +1,12 @@
 document.addEventListener("turbo:load", () => {
     if ($("#menusectionConfig").is(':visible')) {
         // Allergyns
+        function link(cell, formatterParams){
+            var id = cell.getValue();
+            var name = cell.getRow();
+            var rowData = cell.getRow().getData("data").name;
+            return "<a class='link-dark' href='/allergyns/"+id+"/edit'>"+rowData+"</a>";
+        }
         var allergynTable = new Tabulator("#allergyn-table", {
           dataLoader: false,
           maxHeight:"100%",
@@ -8,12 +14,7 @@ document.addEventListener("turbo:load", () => {
           layout:"fitDataStretch",
           ajaxURL: '/allergyns.json',
           columns: [
-          {
-            title:"Name", field:"id", responsive:0, formatter:"link", formatterParams: {
-                labelField:"name",
-                urlPrefix:"/allergyns/",
-            }
-          },
+          {title:"Name", field:"id", responsive:0, formatter:link, hozAlign:"left"},
           {title:"Description", field:"description", responsive:5},
           {title:"Symbol", field:"symbol", responsive:0, hozAlign:"right", headerHozAlign:"right" }
           ],
