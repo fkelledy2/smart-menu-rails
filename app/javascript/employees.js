d39d2bc3f2d999db5eb349883672955eedf01556
fkelledy2 <fkelledy@gmail.com>
2024-06-08 13:49:32 +0100

    Update menu config layout

--- app/javascript/employees.js
+++ app/javascript/employees.js
@@ -35,11 +35,13 @@ document.addEventListener("turbo:load", () => {
           layout:"fitDataStretch",
           ajaxURL: '/restaurants/'+restaurantId+'/employees.json',
           columns: [
-          {
-             formatter:"rowSelection", titleFormatter:"rowSelection", responsive:0, width: 40, frozen:true, headerHozAlign:"left", hozAlign:"left", headerSort:false, cellClick:function(e, cell) {
-                cell.getRow().toggleSelect();
-             }
-          },
+              {
+                  formatter:"rowSelection", titleFormatter:"rowSelection", width: 30, frozen:true, headerHozAlign:"left", hozAlign:"left", headerSort:false, cellClick:function(e, cell) {
+                      cell.getRow().toggleSelect();
+                  }
+              },
+              { rowHandle:true, formatter:"handle", headerSort:false, frozen:true, responsive:0, width:30, minWidth:30 },
+              { title:"", field:"sequence", visible:false, formatter:"rownum", responsive:5, hozAlign:"right", headerHozAlign:"right", headerSort:false },
           {title:"Name", field:"id", responsive:0, formatter:link, hozAlign:"left"},
           {title:"Role", field:"role", responsive:5, hozAlign:"right", headerHozAlign:"right" },
           {title:"Status", field:"status", responsive:0, minWidth: 100, hozAlign:"right", headerHozAlign:"right" }
