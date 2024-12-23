ad58729cf1e389ab452a9d09e68a7744d7858622
fkelledy2 <fkelledy@gmail.com>
2024-12-23 20:10:42 +0100

    Segment.com integration.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,20 @@ class HomeController < ApplicationController
   layout "marketing", :only => [ :index, :terms, :privacy ]
 
   def index
+      Analytics.track(
+          event: 'home.index'
+      )
   end
 
   def terms
+      Analytics.track(
+          event: 'home.terms'
+      )
   end
 
   def privacy
+      Analytics.track(
+          event: 'home.privacy'
+      )
   end
 end
