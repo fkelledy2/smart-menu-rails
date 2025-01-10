12478ea875159d75a1f41628bcdf87b37ad40067
fkelledy2 <fkelledy@gmail.com>
2025-01-10 22:41:05 +0100

    Pay integration and QR code setip.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
             event: 'home.index'
           )
       end
-      @demoMenu = Menu.find(id: 3)
+      @demoMenu = Menu.where(id: 3)
       if @demoMenu
           @qrDemoURL = Rails.application.routes.url_helpers.menu_url(@demoMenu, :host => request.host_with_port)
           if request.host != 'localhost'
