e7f2e66cd343f989654b72e68e27c6254596aa04
fkelledy2 <fkelledy@gmail.com>
2025-02-08 17:39:40 +0100

    Adding playlist functionality.

--- app/jobs/spotify_sync_job.rb
+++ app/jobs/spotify_sync_job.rb
@@ -6,14 +6,19 @@ class SpotifySyncJob
 
   def perform(*args)
       puts 'SpotifySyncJob'
-      puts args
       @restaurant = Restaurant.find_by_id(args[0])
+      puts @restaurant.name
+      puts @restaurant.spotifyuserid
       if @restaurant.spotifyuserid != nil
             # '31jr3waqr4jdel5x7p:wbr6iikzim'
             me = RSpotify::User.find(@restaurant.spotifyuserid)
+            puts me
             Track.where(restaurant: @restaurant).destroy_all
+            puts 'delete.tracks'
             me.playlists.each do |playlist|
+                puts playlist
                 playlist.tracks.each do |track|
+                    puts track
                     album = RSpotify::Album.find(track.album.id)
                     if album != nil
                         puts album.name
