b04bc280cd3fa1ed65bfdb230cfb38d5afa02a2e
fkelledy2 <fkelledy@gmail.com>
2025-02-08 19:15:30 +0100

    Adding playlist functionality.

--- app/jobs/spotify_sync_job.rb
+++ app/jobs/spotify_sync_job.rb
@@ -16,28 +16,30 @@ class SpotifySyncJob
             Track.where(restaurant: @restaurant).destroy_all
             puts 'delete.tracks'
             me.playlists.each do |playlist|
-                puts playlist
-                playlist.tracks.each do |track|
-                    puts track
-                    album = RSpotify::Album.find(track.album.id)
-                    if album != nil
-                        puts album.name
-                        album.tracks.each do |track|
-                                puts track.name
-                                @track = Track.new
-                                @track.restaurant = @restaurant
-                                @track.externalid = track.id
-                                @track.description = album.name
-                                @track.name = track.name
-                                @track.status = 0
-                                @pj = track.album.images
-                                puts @pj[0]['url']
-                                @track.image = @pj[0]['url']
-                                @track.artist = track.artists[0].name
-                                @track.save
-                        end
+                puts playlist.name
+#                 if playlist.id == '25862YMjra6u07FoPSc7UI'
+                    playlist.tracks.each do |track|
+                        puts track
+                                    puts track.name
+                                    @track = Track.new
+                                    @track.restaurant = @restaurant
+                                    @track.externalid = track.id
+                                    @track.description = track.album.name
+                                    @track.name = track.name
+                                    @track.status = 0
+                                    @pj = track.album.images
+                                    puts @pj[0]['url']
+                                    @track.image = @pj[0]['url']
+                                    @track.artist = track.artists[0].name
+                                    @track.save
+#                         album = RSpotify::Album.find(track.album.id)
+#                         if album != nil
+#                             puts album.name
+#                             album.tracks.each do |track|
+#                             end
+#                         end
                     end
-                end
+#                 end
             end
       end
   end
