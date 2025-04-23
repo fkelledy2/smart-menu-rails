cee3079c8ac21a603733ef8ba75f9005d0bd8dc8
fkelledy2 <fkelledy@gmail.com>
2025-04-23 21:17:09 +0200

    Email condig for Devise

--- app/javascript/metrics.js
+++ app/javascript/metrics.js
@@ -3,14 +3,17 @@ document.addEventListener("turbo:load", () => {
     $.get( "/metrics.json", function(data) {
       var currentMetrics = data[0];
       var numberOfRestaurants = currentMetrics.numberOfRestaurants;
-      $('#metrics-numberOfRestaurants').html(numberOfRestaurants);
-      var numberOfMenus = currentMetrics.numberOfMenus;
-      $('#metrics-numberOfMenus').html(numberOfMenus);
-//      var numberOfMenusItems = currentMetrics.numberOfMenusItems;
-//      $('#metrics-numberOfMenusItems').html(numberOfMenusItems);
-      var numberOfOrders = currentMetrics.numberOfOrders;
-      $('#metrics-numberOfOrders').html(numberOfOrders);
-      var totalOrderValue = currentMetrics.totalOrderValue;
-      $('#metrics-totalOrderValue').html(parseFloat(totalOrderValue).toFixed(0));
+      if( numberOfRestaurants > 100 ) {
+          $('#metrics-numberOfRestaurants').html(numberOfRestaurants);
+          var numberOfMenus = currentMetrics.numberOfMenus;
+          $('#metrics-numberOfMenus').html(numberOfMenus);
+          var numberOfOrders = currentMetrics.numberOfOrders;
+          $('#metrics-numberOfOrders').html(numberOfOrders);
+          var totalOrderValue = currentMetrics.totalOrderValue;
+          $('#metrics-totalOrderValue').html(parseFloat(totalOrderValue).toFixed(0));
+      } else {
+          $('#metrics-table').hide();
+          $('#testimonials').hide();
+      }
     });
 })
