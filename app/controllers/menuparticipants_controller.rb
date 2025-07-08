5cd434ede8fdbc6c3574430e789be83f46f6124e
fkelledy2 <fkelledy@gmail.com>
2025-07-08 21:41:53 +0200

    Remove caching

--- app/controllers/menuparticipants_controller.rb
+++ app/controllers/menuparticipants_controller.rb
@@ -65,15 +65,51 @@ class MenuparticipantsController < ApplicationController
   private
 
     def broadcastPartials()
-        @menuparticipant = Menuparticipant.where( sessionid: session.id.to_s ).first
-        if @menuparticipant && @ordrparticipant
-            @ordrparticipant.preferredlocale = @menuparticipant.preferredlocale
+        if menuparticipant.smartmenu.restaurant.currency
+            restaurantCurrency = ISO4217::Currency.from_code(ordr.menu.restaurant.currency)
+        else
+            restaurantCurrency = ISO4217::Currency.from_code('USD')
         end
+        menuparticipant = Menuparticipant.where( sessionid: session.id.to_s ).first
+        allergyns = Allergyn.where( restaurant_id: menuparticipant.smartmenu.restaurant.id )
+        fullRefresh = false
         partials = {
-            fullPageRefresh: { refresh: true }
+            context: ApplicationController.renderer.render(
+                partial: 'smartmenus/showContext',
+                locals: { order: nil, menu: menuparticipant.smartmenu.menu, ordrparticipant: nil, tablesetting: menuparticipant.smartmenu.tablesetting, menuparticipant: menuparticipant, current_employee: @current_employee }
+            ),
+            modals: ApplicationController.renderer.render(
+                partial: 'smartmenus/showModals',
+                locals: { order: nil, menu: menuparticipant.smartmenu.menu, ordrparticipant: nil, tablesetting: menuparticipant.smartmenu.tablesetting, menuparticipant: menuparticipant, restaurantCurrency: restaurantCurrency, current_employee: @current_employee }
+            ),
+            menuContentStaff: ApplicationController.renderer.render(
+                partial: 'smartmenus/showMenuContentStaff',
+                locals: { order: nil, menu: menuparticipant.smartmenu.menu, allergyns: allergyns, restaurantCurrency: restaurantCurrency, ordrparticipant: ordrparticipant, menuparticipant: menuparticipant }
+            ),
+            menuContentCustomer: ApplicationController.renderer.render(
+                partial: 'smartmenus/showMenuContentCustomer',
+                locals: { order: nil, menu: menuparticipant.smartmenu.menu, allergyns: allergyns, restaurantCurrency: restaurantCurrency, ordrparticipant: ordrparticipant, menuparticipant: menuparticipant }
+            ),
+            orderCustomer: ApplicationController.renderer.render(
+                partial: 'smartmenus/orderCustomer',
+                locals: { order: nil, menu: menuparticipant.smartmenu.menu, restaurant: menuparticipant.smartmenu.restaurant, tablesetting: menuparticipant.smartmenu.tablesetting, ordrparticipant: nil }
+            ),
+            orderStaff: ApplicationController.renderer.render(
+                partial: 'smartmenus/orderStaff',
+                locals: { order: nil, menu: menuparticipant.smartmenu.menu, restaurant: menuparticipant.smartmenu.restaurant, tablesetting: menuparticipant.smartmenu.tablesetting, ordrparticipant: nil }
+            ),
+            tableLocaleSelectorStaff: ApplicationController.renderer.render(
+                partial: 'smartmenus/showTableLocaleSelectorStaff',
+                locals: { menu: menuparticipant.smartmenu.menu, restaurant: menuparticipant.smartmenu.restaurant, tablesetting: menuparticipant.smartmenu.tablesetting, ordrparticipant: nil, menuparticipant: menuparticipant }
+            ),
+            tableLocaleSelectorCustomer: ApplicationController.renderer.render(
+                partial: 'smartmenus/showTableLocaleSelectorCustomer',
+                locals: { menu: menuparticipant.smartmenu.menu, restaurant: menuparticipant.smartmenu.restaurant, tablesetting: menuparticipant.smartmenu.tablesetting, ordrparticipant: nil, menuparticipant: menuparticipant }
+            ),
+            fullPageRefresh: { refresh: fullRefresh }
         }
-        ActionCable.server.broadcast("ordr_"+@menuparticipant.smartmenu.slug+"_channel", partials)
-     end
+        ActionCable.server.broadcast("ordr_"+menuparticipant.smartmenu.slug+"_channel", partials)
+    end
 
     # Use callbacks to share common setup or constraints between actions.
     def set_menuparticipant
