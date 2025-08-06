a7e3fbf13f21c867f78fca7df917b90f07ba088f
fkelledy2 <fkelledy@gmail.com>
2025-08-06 19:06:52 +0100

    Identity caching and JS fixes

--- app/models/menuitem_allergyn_mapping.rb
+++ app/models/menuitem_allergyn_mapping.rb
@@ -1,4 +1,10 @@
 class MenuitemAllergynMapping < ApplicationRecord
+  include IdentityCache
+  
   belongs_to :menuitem
   belongs_to :allergyn
+  
+  # IdentityCache configuration
+  cache_belongs_to :menuitem
+  cache_belongs_to :allergyn
 end
