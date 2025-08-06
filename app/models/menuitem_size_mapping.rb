781c59942b5ae33385c8517d667552ab8f342f1f
fkelledy2 <fkelledy@gmail.com>
2025-08-06 19:38:05 +0100

    Identity caching and JS fixes

--- app/models/menuitem_size_mapping.rb
+++ app/models/menuitem_size_mapping.rb
@@ -1,8 +1,19 @@
 class MenuitemSizeMapping < ApplicationRecord
+  include IdentityCache
+  
   belongs_to :menuitem
   belongs_to :size
+  
+  # IdentityCache configuration
+  cache_index :id
+  cache_index :menuitem_id
+  cache_index :size_id
+  
+  # Cache associations
+  cache_belongs_to :menuitem
+  cache_belongs_to :size
 
   def sizeName
-      size.name
+    size.name
   end
 end
