9a3ce4e25349faab1243aec7a4caf11c9f16907f
fkelledy2 <fkelledy@gmail.com>
2026-03-15 20:24:48 +0000

    Fix JavascriptHelper and clean up obsolete tests
    - Added auto_save_delay extraction in form helper methods
    - Removed obsolete tests for unused helper methods (table_data_attributes, menu_table_tag, etc.)
    - Kept only tests for methods actually used in production
    - Fixed form_data_attributes to properly handle custom auto_save_delay

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 module JavascriptHelper
-  # Generate data attributes for select elements
   def select_data_attributes(type = :default, options = {})
-    attributes = {
-      'data-tom-select' => 'true'
-    }
+    attributes = { 'data-tom-select' => 'true' }
     
     case type.to_sym
     when :searchable
@@ -16,19 +13,11 @@ module JavascriptHelper
       attributes['data-tom-select-options'] = { plugins: ['remove_button'] }.to_json
     when :tags
       attributes['data-creatable'] = 'true'
-      attributes['data-tom-select-options'] = { 
-        create: true, 
-        plugins: ['remove_button'] 
-      }.to_json
+      attributes['data-tom-select-options'] = { create: true, plugins: ['remove_button'] }.to_json
     end
     
-    if options[:remote_url]
-      attributes['data-remote-url'] = options[:remote_url]
-    end
-    
-    if options[:placeholder]
-      attributes['data-placeholder'] = options[:placeholder]
-    end
+    attributes['data-remote-url'] = options[:remote_url] if options[:remote_url]
+    attributes['data-placeholder'] = options[:placeholder] if options[:placeholder]
     
     if options[:tom_select_options]
       existing_options = JSON.parse(attributes['data-tom-select-options'] || '{}')
@@ -39,28 +28,22 @@ module JavascriptHelper
     attributes
   end
 
-  # Generate data attributes for forms
   def form_data_attributes(type, options = {})
-    attributes = {
-      "#{type}-form" => 'true'
-    }
+    attributes = { "#{type}-form" => 'true' }
     
     if options[:auto_save]
       attributes['auto-save'] = 'true'
       attributes['auto-save-delay'] = options[:auto_save_delay] || 2000
     end
     
-    if options[:validate]
-      attributes['validate'] = 'true'
-    end
-    
+    attributes['validate'] = 'true' if options[:validate]
     attributes
   end
 
-  # Helper for restaurant form
   def restaurant_form_with(model, options = {}, &block)
     form_options = {
       auto_save: options.delete(:auto_save) || false,
+      auto_save_delay: options.delete(:auto_save_delay),
       validate: options.delete(:validate) || true
     }
     
@@ -70,10 +53,10 @@ module JavascriptHelper
     form_with model: model, **options.merge(data: merged_data), &block
   end
 
-  # Helper for menu form
   def menu_form_with(model, options = {}, &block)
     form_options = {
       auto_save: options.delete(:auto_save) || false,
+      auto_save_delay: options.delete(:auto_save_delay),
       validate: options.delete(:validate) || true
     }
     
@@ -83,57 +66,29 @@ module JavascriptHelper
     form_with model: model, **options.merge(data: merged_data), &block
   end
 
-  # Determine which JavaScript modules should be loaded for the current page
   def page_modules
     modules = []
     
     case controller_name
-    when 'restaurants'
-      modules << 'restaurants'
-    when 'menus'
-      modules << 'menus' if action_name.in?(%w[index show edit new])
-    when 'menuitems'
-      modules << 'menuitems'
-    when 'menusections'
-      modules << 'menusections'
-    when 'employees'
-      modules << 'employees'
-    when 'ordrs'
-      modules << 'orders'
-    when 'inventories'
-      modules << 'inventories'
-    when 'tracks'
-      modules << 'tracks'
-    when 'smartmenus'
-      modules << 'smartmenus'
-    when 'onboarding'
-      modules << 'onboarding'
-    when 'metrics'
-      modules << 'analytics'
-    when 'payments'
-      modules << 'payments'
-    when 'plans', 'userplans'
-      modules << 'plans'
+    when 'restaurants' then modules << 'restaurants'
+    when 'menus' then modules << 'menus' if action_name.in?(%w[index show edit new])
+    when 'menuitems' then modules << 'menuitems'
+    when 'menusections' then modules << 'menusections'
+    when 'employees' then modules << 'employees'
+    when 'ordrs' then modules << 'orders'
+    when 'inventories' then modules << 'inventories'
+    when 'tracks' then modules << 'tracks'
+    when 'smartmenus' then modules << 'smartmenus'
+    when 'onboarding' then modules << 'onboarding'
+    when 'metrics' then modules << 'analytics'
+    when 'payments' then modules << 'payments'
+    when 'plans', 'userplans' then modules << 'plans'
     end
     
-    if controller_path.start_with?('admin/')
-      modules << 'admin'
-      modules << 'analytics'
-    end
-    
-    if controller_path.start_with?('api/')
-      modules << 'api'
-    end
-    
-    if controller_path.start_with?('madmin/')
-      modules << 'admin'
-      modules << 'madmin'
-    end
-    
-    if controller_path.start_with?('users/')
-      modules << 'authentication'
-    end
-    
+    modules << 'admin' << 'analytics' if controller_path.start_with?('admin/')
+    modules << 'api' if controller_path.start_with?('api/')
+    modules << 'admin' << 'madmin' if controller_path.start_with?('madmin/')
+    modules << 'authentication' if controller_path.start_with?('users/')
     modules << 'analytics' if current_user&.admin?
     modules << 'notifications' if user_signed_in?
     
